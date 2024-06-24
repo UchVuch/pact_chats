a771eefd91e89da77b65f6b4a9b35d6421801b58
UchVuch <uchvuch@gmail.com>
2024-06-24 03:39:59 +0300

    feat: added send message and chat max height

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,9 +1,46 @@
 <script setup>
+import { ref } from 'vue'
 import ChatList from '../components/Chat/ChatList.vue'
 import TopBar from '../components/ui/TopBar.vue'
 import MessagesList from '../components/Messages/MessagesList.vue'
 import InputBar from '../components/ui/InputBar.vue'
 
+const messages = ref([
+  {
+    id: 1,
+    text: 'Hi, how are you?',
+    time: '10:00',
+    date: 1719007200,
+    status: true,
+    me: false
+  },
+  {
+    id: 2,
+    text: `I'm okay, what about you?`,
+    time: '15:00',
+    date: 1719110800,
+    status: true,
+    me: false
+  },
+  {
+    id: 3,
+    text: 'Привет, все работает',
+    time: '10:12',
+    date: 1719190800,
+    status: false,
+    me: true
+  }
+])
+const sendNewMessage = (text) => {
+  messages.value.push({
+    id: messages.value.length + 1,
+    text,
+    time: '10:00',
+    date: Math.floor(new Date().getTime() / 1000),
+    status: false,
+    me: messages.value.length % 2 ? true : false
+  })
+}
 </script>
 
 <template>
@@ -13,8 +50,8 @@ import InputBar from '../components/ui/InputBar.vue'
       <TopBar/>
       <div class="chat__content">
         <div class="chat__content-inner">
-          <MessagesList class="chat__list"/>
-          <InputBar/>
+          <MessagesList class="chat__list" :messages="messages"/>
+          <InputBar @send-message="sendNewMessage"/>
         </div>
       </div>
     </div>
@@ -30,20 +67,25 @@ import InputBar from '../components/ui/InputBar.vue'
 }
 
 .chat {
-  flex-grow: 1;
   display: flex;
   flex-direction: column;
 
   &__content {
     flex-grow: 1;
     display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
     background-color: $iceberg-blue;
     padding: 24px;
   }
 
   &__content-inner {
+    flex-grow: 1;
     width: 100%;
     align-self: flex-end;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
   }
 
   &__list {
